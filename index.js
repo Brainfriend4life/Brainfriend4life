32137e2875852fdeb2dd3bad3c5874386c7e2d12
brainfriend4life <emmanueloghenevovwero85@gmail.com>
2025-07-15 18:39:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+  window.addEventListener("DOMContentLoaded", function () {
+    const backToTopBtn = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 200) {
+        backToTopBtn.style.display = "block";
+      } else {
+        backToTopBtn.style.display = "none";
+      }
+    });
+
+    backToTopBtn.addEventListener("click", function () {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  });
+
